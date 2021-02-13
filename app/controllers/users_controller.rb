9960841e2ba29a6a612c4baa771132f75711f181
Marti D <me@martidolce.com>
2021-02-13 18:19:51 -0500

    The user profile includes: - login with dynamic features - Enabling sessions - Ability to CRUD personal data
    Signed-off-by: Marti D <me@martidolce.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,32 @@
 require './config/environment'
 
 class UsersController < ApplicationController
-  # user can sign up
+
+  # Users Home
+  get "/users" do
+
+  end
+
+  # New
   get '/signup' do
     redirect "/users/#{current_user.id}" if logged_in?
     erb :"users/new"
   end
 
+  # Create
   post '/signup' do
-    user = User.new()
+    user = User.new(params)
 
     # Won't be saved to db unless user filled out password
-    if !user.username || !user.password
-      @error = "Please enter a username and password!"
+    if user.errors
+      @error = "Please complete all fields!"
       erb :'/users/new'
     # Check if there is an existing username
     elsif User.find_by(username: user.username)
       @error = "This username already exists. Please create a new username."
       erb :'/users/new'
     else
+      user = User.create(params)
       user.save
       session[:user_id] = user.id
       redirect '/'
@@ -27,8 +35,38 @@ class UsersController < ApplicationController
 
   get '/users/:id' do
     redirect '/' if !logged_in?
-    @user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:id])
     # @problems = @user.problems.order('date desc')
     erb :"/users/dashboard"
+  end
+
+  # show
+  get '/users/:id' do
+    user = User.find(params[:id])
+    erb :"/users/show"
+  end
+
+  # edit
+  get '/users/:id/edit' do
+    user = User.find(params[:id])
+    erb :"/users/edit.html"
+  end
+
+  # update
+  patch '/users/:id' do
+    user = User.find(params[:id])
+    user.update(params[:User])
+    redirect to "/users/#{user.id}"
+  end
+
+  # destroy
+  delete '/users/:id' do
+    User.destroy(params[:id])
+    redirect to '/users'
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect to '/users'
   end
 end
